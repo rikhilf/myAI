eae4654a39724161229d0c0f2402b5d1370b1ba1
Rikhil Fellner <rfellner@unc.edu>
2025-02-26 15:40:22 -0500

    Update identity.ts
    Add owner name and description

--- configuration/identity.ts
+++ configuration/identity.ts
@@ -1,5 +1,5 @@
-export const OWNER_NAME: string = `[OWNER NAME]`;
-export const OWNER_DESCRIPTION: string = `[OWNER DESCRIPTION]`;
+export const OWNER_NAME: string = `Rikhil Fellner`;
+export const OWNER_DESCRIPTION: string = `UNC computer science student`;
 
 export const AI_NAME: string = `[AI NAME]`;
 export const AI_TONE: string = `[AI TONE]`;
