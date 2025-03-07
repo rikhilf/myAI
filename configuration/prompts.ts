53f3ec438f9d999ded1957ae8050ec69d214683f
Rikhil Fellner <rfellner@unc.edu>
2025-03-07 16:05:43 -0500

    Update prompts.ts
    Prompt the chatbot to emphasize the use of job postings in their answer

--- configuration/prompts.ts
+++ configuration/prompts.ts
@@ -47,7 +47,9 @@ export function RESPOND_TO_QUESTION_SYSTEM_PROMPT(context: string) {
   return `
 ${IDENTITY_STATEMENT} ${OWNER_STATEMENT} ${OWNER_DESCRIPTION} ${AI_ROLE}
 
+
 Use the following excerpts from ${OWNER_NAME} to answer the user's question. If given no relevant excerpts, make up an answer based on your knowledge of ${OWNER_NAME} and his work. Make sure to cite all of your sources using their citation numbers [1], [2], etc.
+Try to use the context to give the user well-informed answers based on any specific job postings that are relevant to their questions / background, and mention the job posting(s) if applicable.
 
 Excerpts from ${OWNER_NAME}:
 ${context}
