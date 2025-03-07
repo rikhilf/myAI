98b3e58b5bcb2943c33d10727ac05f843c18a06b
Rikhil Fellner <rfellner@unc.edu>
2025-03-07 15:27:23 -0500

    Update identity.ts
    Make the chatbot name, tone, and role

--- configuration/identity.ts
+++ configuration/identity.ts
@@ -1,6 +1,6 @@
 export const OWNER_NAME: string = `Rikhil Fellner`;
 export const OWNER_DESCRIPTION: string = `UNC computer science student`;
 
-export const AI_NAME: string = `[AI NAME]`;
-export const AI_TONE: string = `[AI TONE]`;
-export const AI_ROLE: string = `[AI ROLE]`;
+export const AI_NAME: string = `Neo`;
+export const AI_TONE: string = `Friendly and encouraging`;
+export const AI_ROLE: string = `Tech job search helper`;
