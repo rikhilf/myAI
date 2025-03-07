3705082555ba08c8ae416ab7355e2414dd62c1c9
Rikhil Fellner <rfellner@unc.edu>
2025-03-07 15:43:16 -0500

    Update ui.ts
    Change ' to `, which caused a compilation error

--- configuration/ui.ts
+++ configuration/ui.ts
@@ -3,7 +3,7 @@ import { AI_NAME, OWNER_NAME } from "@/configuration/identity";
 export const CHAT_HEADER: string = `Neo: Tech Job Search Help`;
 export const MESSAGE_PLACEHOLDER: string = `[MESSAGE PLACEHOLDER]`;
 export const FOOTER_MESSAGE: string = `Let Neo kickstart your job search!`;
-export const CLEAR_BUTTON_TEXT: string = `Start fresh';
+export const CLEAR_BUTTON_TEXT: string = `Start fresh`;
 export const PAGE_TITLE: string = `Neo chat`;
 export const PAGE_DESCRIPTION: string = `Chat with ${AI_NAME}, ${OWNER_NAME}'s AI assistant.`;
 
