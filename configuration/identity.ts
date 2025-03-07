bef5e5529e88ca069d919241d783bf61360bd850
Rikhil Fellner <rfellner@unc.edu>
2025-03-07 17:22:16 -0500

    Update identity.ts
    Add 'new grad' to identity

--- configuration/identity.ts
+++ configuration/identity.ts
@@ -3,4 +3,4 @@ export const OWNER_DESCRIPTION: string = `UNC computer science student`;
 
 export const AI_NAME: string = `Neo`;
 export const AI_TONE: string = `Friendly and encouraging`;
-export const AI_ROLE: string = `Tech job search helper`;
+export const AI_ROLE: string = `New Grad tech job search helper`;
