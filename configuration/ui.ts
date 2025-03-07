9c0e0cd783295ad1ba43edd31700f8a0efd539ea
Rikhil Fellner <rfellner@unc.edu>
2025-03-07 15:34:57 -0500

    Update ui.ts
    Add ui placeholders

--- configuration/ui.ts
+++ configuration/ui.ts
@@ -1,10 +1,10 @@
 import { AI_NAME, OWNER_NAME } from "@/configuration/identity";
 
-export const CHAT_HEADER: string = `[CHAT HEADER]`;
+export const CHAT_HEADER: string = `Neo: Tech Job Search Help`;
 export const MESSAGE_PLACEHOLDER: string = `[MESSAGE PLACEHOLDER]`;
-export const FOOTER_MESSAGE: string = `[FOOTER MESSAGE]`;
-export const CLEAR_BUTTON_TEXT: string = `[CLEAR BUTTON TEXT]`;
-export const PAGE_TITLE: string = `[PAGE TITLE]`;
+export const FOOTER_MESSAGE: string = `Let Neo kickstart your job search!`;
+export const CLEAR_BUTTON_TEXT: string = `Start fresh';
+export const PAGE_TITLE: string = `Neo chat`;
 export const PAGE_DESCRIPTION: string = `Chat with ${AI_NAME}, ${OWNER_NAME}'s AI assistant.`;
 
 export const EMPTY_CITATION_MESSAGE: string = "Unspecified source";
