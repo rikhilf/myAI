98a5c07153bbc858222f2d300e5495078716c85a
Rikhil Fellner <rfellner@unc.edu>
2025-03-07 17:22:47 -0500

    Update ui.ts
    Add 'new grad' to the header

--- configuration/ui.ts
+++ configuration/ui.ts
@@ -1,6 +1,6 @@
 import { AI_NAME, OWNER_NAME } from "@/configuration/identity";
 
-export const CHAT_HEADER: string = `Neo: Tech Job Search Help`;
+export const CHAT_HEADER: string = `Neo: New Grad Tech Job Search Help`;
 export const MESSAGE_PLACEHOLDER: string = `[MESSAGE PLACEHOLDER]`;
 export const FOOTER_MESSAGE: string = `Let Neo kickstart your job search!`;
 export const CLEAR_BUTTON_TEXT: string = `Start fresh`;
